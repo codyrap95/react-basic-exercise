fedd471dea85ad3abf6d3370163d8ea6d53287db
Cody <codydjay@gmail.com>
2021-08-04 12:04:29 +0300

    Exercise Done

--- src/components/RandomUser.js
+++ src/components/RandomUser.js
@@ -2,22 +2,39 @@ import React from "react";
 import axios from "axios";
 
 export default function RandomUser() {
-  const [fetchData, setFetchData] = React.useState("");
+  const [fetchData, setFetchData] = React.useState([]);
   const fetchHandler = async () => {
     try {
-      setFetchData("Loading...");
       const response = await axios.get("https://randomuser.me/api");
-      const user = response.data.results[0];
-      setFetchData(JSON.stringify(user));
+      const users = response.data.results;
+      setFetchData((prev) => {
+        return prev !== [] ? [...prev, ...users] : users;
+      });
     } catch (e) {
       setFetchData("Fetch Failed. Reason: " + e.toString());
     }
   };
-
+  React.useEffect(() => {
+    fetchHandler();
+  }, []);
   return (
     <div>
       <button onClick={fetchHandler}>Fetch Random User</button>
-      <ul>{fetchData}</ul>
+      <ul>
+        {console.log(fetchData)}
+        {fetchData.length === 0 || typeof fetchData !== "object"
+          ? "Loading..."
+          : fetchData.map((user) => (
+              <li key={user.login.uuid}>
+                <p>
+                  <img src={user.picture.thumbnail} alt="user photo" />
+                </p>
+                <p>
+                  {user.name.first} {user.name.last}
+                </p>
+              </li>
+            ))}
+      </ul>
     </div>
   );
 }
